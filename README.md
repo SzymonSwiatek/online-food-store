8ed368df3c8b5545346386fe721609890a4d64ae
SzymonSwiatek <swiatek1996@gmail.com>
2022-10-20 18:07:19 +0200

    search route and component

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
 ## Tasks
 
 1. Add Header
-   a) Generate component
-   b) Add HTML
-   c) Add CSS
-   d) Reset global CSS
+
+   - Generate component
+   - Add HTML
+   - Add CSS
+   - Reset global CSS
 
 2. List Foods
-   a) Create Food model
-   b) Create data.ts
 
-   - Add sample foods
+   - Create Food model
+   - Create data.ts
+     - Add sample foods
 
 3. Add images to assets
 4. Create Food service
 5. Create Home component
-   a) Add TS
-   b) Add HTML
-   c) Add CSS
+   - Add TS
+   - Add HTML
+   - Add CSS
+6. Search
+   - Add method to Food service
+   - Add search route
+   - Show search result in Home component
+   - Generate search comonent
+     - Add to home component
+     - Add TS
+     - Add HTML
+     - Add CSS
--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -9,8 +10,23 @@ import { Food } from 'src/app/shared/models/Food';
 })
 export class HomeComponent implements OnInit {
   foods: Food[] = [];
-  constructor(private foodService: FoodService) {
-    this.foods = foodService.getAll();
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute //listen to the route
+  ) {
+    // this.foods = foodService.getAll();
+
+    activatedRoute.params.subscribe((params) => {
+      if (params['searchTerm']) {
+        //index signature could be change in the tsconfig.json
+        this.foods = this.foodService.getAllFoodsBySearchTerm(
+          params['searchTerm']
+        );
+      } else {
+        this.foods = foodService.getAll();
+        // console.log(params['searchTerm']);
+      }
+    });
   }
 
   ngOnInit(): void {}
