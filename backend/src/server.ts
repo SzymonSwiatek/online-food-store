90c3a58bd8daadebc989f603c5e9366bad76a3ae
SzymonSwiatek <swiatek1996@gmail.com>
2022-12-15 22:50:29 +0100

    connect to mongodb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 
-import foodRouter from './routers/food.router';
+import foodRouter from "./routers/food.router";
 import userRouter from "./routers/user.router";
+import { dbConnect } from "./configs/database.config";
+dbConnect();
+
 const app = express();
 app.use(express.json()); //enable json on express framework
 
@@ -16,12 +22,8 @@ app.use(
   })
 );
 
-
-
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
-
-
 
 const port = 5000;
 app.listen(port, () => {
